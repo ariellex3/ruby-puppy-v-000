8e6792d35efbc7ed026b13abacf830b11e7dc382
Arielle D <domingu4@gmail.com>
2018-11-19 13:41:19 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << @name
+  end
+
+  def self.all
+    @@all.each do |dog|
+       puts dog
+    end
+  end
+
+end
